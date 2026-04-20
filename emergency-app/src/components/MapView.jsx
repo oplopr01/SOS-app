47440b9e30d3f940e511cf82cf2de93c3026d53e
oplopr01 <oplopr01@gmail.com>
2026-04-20 20:33:53 +0530

    changed color of pointer on map diff for user and locations

--- emergency-app/src/components/MapView.jsx
+++ emergency-app/src/components/MapView.jsx
@@ -1,21 +1,34 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 
 function MapView({ lat, lng, services = [] }) {
+
+  const userIcon = new L.Icon({
+    iconUrl: "https://maps.google.com/mapfiles/ms/icons/red-dot.png",
+    iconSize: [32, 32],
+  });
+
+  const serviceIcon = new L.Icon({
+    iconUrl: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+    iconSize: [32, 32],
+  });
   return (
     <MapContainer center={[lat, lng]} zoom={14} style={{ height: "60vh" }}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-      <Marker position={[lat, lng]}>
+      {/* User Location */}
+      <Marker position={[lat, lng]} icon={userIcon}>
         <Popup>You are here</Popup>
       </Marker>
 
-      {services.map((s) => (
-        <Marker key={s.id} position={[s.lat, s.lng]}>
-          <Popup>
-            {s.name} ({s.type})
-          </Popup>
-        </Marker>
-      ))}
+      {/* Nearby Services */}
+      {services
+        .filter((s) => s.lat && s.lng)
+        .map((s) => (
+          <Marker key={s.id} position={[s.lat, s.lng]} icon={serviceIcon}>
+            <Popup>{s.name}</Popup>
+          </Marker>
+        ))}
     </MapContainer>
   );
 }
